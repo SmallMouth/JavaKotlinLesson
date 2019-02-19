61408a0fe2c402e30e45c4c9ac201901a59a031b
SmallMouth <lismallmouth@gmail.com>
2019-02-19 12:24:50 +0800

    Initial commit

---
+++ src/main/java/com/smallmouthlee/Person.java
@@ -0,0 +1,7 @@
+package com.smallmouthlee;
+
+public class Person {
+    public void hello() {
+        System.out.println("Person");
+    }
+}
---
+++ src/main/kotlin/com/smallmouthlee/Hello.kt
@@ -0,0 +1,13 @@
+package com.smallmouthlee
+
+fun main() {
+//    print("hello")
+    val human = Human()
+    human.hello()
+}
+
+class Human {
+    fun hello() {
+        print("hello human")
+    }
+}
